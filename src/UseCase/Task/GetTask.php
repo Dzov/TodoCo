571e30b63701cf6eddc5d212d4f1fe01fc8d714e
Dzov <amelie.haladjian@gmail.com>
2019-03-11 14:08:05 +0100

    feat(task): add get task use case and tsts

---
+++ src/UseCase/Task/GetTask.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\UseCase\Task;
+
+use App\Repository\TaskRepository;
+use App\UseCase\AbstractTaskUseCase;
+
+/**
+ * @author Amélie Haladjian <amelie.haladjian@gmail.com>
+ */
+class GetTask extends AbstractTaskUseCase
+{
+    public function __construct(TaskRepository $repository)
+    {
+        parent::__construct($repository);
+    }
+
+    /**
+     * @throws \App\Exception\Task\TaskNotFoundException
+     */
+    public function execute(int $taskId)
+    {
+        return $this->taskRepository->findById($taskId);
+    }
+}
---
+++ tests/UseCase/Task/GetTaskTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Tests\UseCase\Task;
+
+use App\Tests\Doubles\Task\Entity\TaskStub1;
+use App\Tests\Doubles\Task\Repository\InMemoryTaskRepository;
+use App\UseCase\Task\GetTask;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @author Amélie Haladjian <amelie.haladjian@gmail.com>
+ */
+class GetTaskTest extends TestCase
+{
+    use AssertTaskTrait;
+
+    const INVALID_TASK_ID = -1;
+
+    /**
+     * @var GetTask
+     */
+    private $useCase;
+
+    /**
+     * @test
+     */
+    public function withInvalidIdExecuteShouldThrowException()
+    {
+        $this->expectException('App\Exception\Task\TaskNotFoundException');
+        $this->useCase->execute(self::INVALID_TASK_ID);
+    }
+
+    /**
+     * @test
+     */
+    public function validIdExecuteShouldReturnTask()
+    {
+        $actual = $this->useCase->execute(TaskStub1::ID);
+
+        $this->assertTask(new TaskStub1(), $actual);
+    }
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->useCase = new GetTask(new InMemoryTaskRepository([TaskStub1::ID => new TaskStub1()]));
+    }
+}
