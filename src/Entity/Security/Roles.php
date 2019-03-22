83a6da0f1c61f9f11a55445fc4f018652944613b
Dzov <amelie.haladjian@gmail.com>
2019-03-22 18:44:52 +0100

    feat(authorization): add roles, restrict user managment to admin

---
+++ src/Entity/Security/Roles.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Entity\Security;
+
+/**
+ * @author Amélie Haladjian <amelie.haladjian@gmail.com>
+ */
+final class Roles
+{
+    const ROLE_ADMIN = 'ROLE_ADMIN';
+
+    const ROLE_USER  = 'ROLE_USER';
+}
