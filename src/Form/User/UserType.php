e1cfa8bca639aea940d946d339b3a1e95638b5ae
Dzov <amelie.haladjian@gmail.com>
2019-03-22 09:40:38 +0100

    feat(user forms): format forms

--- src/Form/User/UserType.php
+++ src/Form/User/UserType.php
@@ -14,15 +14,21 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('username', TextType::class, ['label' => "Nom d'utilisateur"])
-            ->add('password', RepeatedType::class, [
-                'type' => PasswordType::class,
-                'invalid_message' => 'Les deux mots de passe doivent correspondre.',
-                'required' => true,
-                'first_options'  => ['label' => 'Mot de passe'],
-                'second_options' => ['label' => 'Tapez le mot de passe à nouveau'],
-            ])
-            ->add('email', EmailType::class, ['label' => 'Adresse email'])
-        ;
+            ->add('username', TextType::class, ['label' => "Nom d'utilisateur", 'attr' => ['class' => 'form-control']])
+            ->add(
+                'password',
+                RepeatedType::class,
+                [
+                    'type'            => PasswordType::class,
+                    'invalid_message' => 'Les deux mots de passe doivent correspondre.',
+                    'required'        => true,
+                    'first_options'   => ['label' => 'Mot de passe', 'attr' => ['class' => 'form-control'],],
+                    'second_options'  => [
+                        'label' => 'Tapez le mot de passe à nouveau',
+                        'attr'  => ['class' => 'form-control'],
+                    ],
+                ]
+            )
+            ->add('email', EmailType::class, ['label' => 'Adresse email', 'attr' => ['class' => 'form-control'],]);
     }
 }
