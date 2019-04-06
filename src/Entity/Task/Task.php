b3cfe99d714d1c65eab954f82be26b3a5af52e7b
Dzov <amelie.haladjian@gmail.com>
2019-04-06 23:25:59 +0200

    refacto(*): fix namespace

--- src/Entity/Task/Task.php
+++ src/Entity/Task/Task.php
@@ -8,7 +8,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Validator\Constraints as Assert;
 
 /**
- * @ORM\Entity(repositoryClass="App\Repository\TaskRepository")
+ * @ORM\Entity(repositoryClass="App\Repository\Task\TaskRepository")
  * @ORM\Table
  */
 class Task
