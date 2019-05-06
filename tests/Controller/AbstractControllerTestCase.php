565d2cc410b333146745c76813cb706079f11984
Dzov <amelie.haladjian@gmail.com>
2019-05-06 10:38:22 +0200

    feat(tests): test all routes

---
+++ tests/Controller/AbstractControllerTestCase.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Client;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * @author Amélie Haladjian <amelie.haladjian@gmail.com>
+ */
+abstract class AbstractControllerTestCase extends WebTestCase
+{
+    const PASSWORD = 'test';
+
+    const USERNAME = 'UserStub1';
+
+    /**
+     * @var Client
+     */
+    protected $client;
+
+    protected function assertStatus(int $expectedStatus = Response::HTTP_OK)
+    {
+        $this->assertSame($expectedStatus, $this->client->getResponse()->getStatusCode());
+    }
+
+    protected function login(): \Symfony\Component\DomCrawler\Crawler
+    {
+        $crawler = $this->client->request('GET', '/login');
+
+        $form = $crawler->selectButton('Se connecter')->form();
+
+        $form['username'] = self::USERNAME;
+        $form['password'] = self::PASSWORD;
+
+        $this->client->submit($form);
+
+        return $crawler;
+    }
+
+    protected function setUp()
+    {
+        $this->client = self::createClient();
+    }
+}
---
+++ tests/Controller/ControllerTestCase.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * @author Amélie Haladjian <amelie.haladjian@gmail.com>
+ */
+class ControllerTestCase extends AbstractControllerTestCase
+{
+    const TASK_ID = 12;
+
+    const USER_ID = 12;
+
+    public function routes()
+    {
+        $homepage = ['GET', '/'];
+        $login = ['GET', '/login'];
+        $getTasks = ['GET', '/tasks'];
+        $createTask = ['GET', '/tasks/create'];
+        $editTask = ['GET', '/tasks/' . self::TASK_ID . '/edit'];
+        $prioritizeTask = ['GET', '/tasks/' . self::TASK_ID . '/prioritize', Response::HTTP_FOUND];
+        $toggleTask = ['GET', '/tasks/' . self::TASK_ID . '/toggle', Response::HTTP_FOUND];
+        $deleteTask = ['GET', '/tasks/' . self::TASK_ID . '/delete', Response::HTTP_FORBIDDEN];
+        $getUsers = ['GET', '/admin/users'];
+        $createUser = ['GET', '/admin/users/create'];
+        $editUser = ['GET', '/admin/users/' . self::USER_ID . '/edit'];
+
+        return [
+            $homepage,
+            $login,
+            $getTasks,
+            $createTask,
+            $editTask,
+            $prioritizeTask,
+            $toggleTask,
+            $deleteTask,
+            $getUsers,
+            $createUser,
+            $editUser,
+        ];
+    }
+
+    /**
+     * @test
+     * @dataProvider routes
+     */
+    public function loggedInShouldRenderRequestedPage(
+        string $method,
+        string $uri,
+        int $expectedStatus = Response::HTTP_OK
+    ) {
+        $this->login();
+
+        $this->client->request($method, $uri);
+
+        $this->assertStatus($expectedStatus);
+    }
+}
